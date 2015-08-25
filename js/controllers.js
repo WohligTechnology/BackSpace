2ab5b534a7e25ee8daea99c28b2b5eb6629a4757
jagruti181 <patiljagruti181@gmail.com>
2015-08-25 18:10:14 +0530

    ooh ready thud

--- js/controllers.js
+++ js/controllers.js
@@ -5,50 +5,152 @@ var phonecatControllers = angular.module('phonecatControllers', ['templateservic
 window.uploadUrl = 'http://104.197.23.70/user/uploadfile';
 //window.uploadUrl = 'http://192.168.2.22:1337/user/uploadfile';
 //window.uploadUrl = 'http://localhost:1337/user/uploadfile';
-phonecatControllers.controller('home', function ($scope, TemplateService, NavigationService, $routeParams, $location) {
-    $scope.template = TemplateService;
-    $scope.menutitle = NavigationService.makeactive("Dashboard");
-    TemplateService.title = $scope.menutitle;
-    TemplateService.submenu = "";
-    TemplateService.content = "views/dashboard.html";
-    TemplateService.list = 1;
-    $scope.navigation = NavigationService.getnav();
-    NavigationService.countUser(function (data, status) {
-        $scope.user = data;
-    });
+phonecatControllers.controller('home', function($scope, TemplateService, NavigationService, $routeParams, $location) {
+  $scope.template = TemplateService;
+  $scope.menutitle = NavigationService.makeactive("Dashboard");
+  TemplateService.title = $scope.menutitle;
+  TemplateService.submenu = "";
+  TemplateService.content = "views/dashboard.html";
+  TemplateService.list = 1;
+  $scope.navigation = NavigationService.getnav();
+  //  NavigationService.countUser(function(data, status) {
+  //    $scope.user = data;
+  //  });
 });
-phonecatControllers.controller('login', function ($scope, TemplateService, NavigationService, $routeParams, $location) {
-    $scope.template = TemplateService;
-    TemplateService.content = "views/login.html";
-    TemplateService.list = 3;
+phonecatControllers.controller('login', function($scope, TemplateService, NavigationService, $routeParams, $location) {
+  $scope.template = TemplateService;
+  TemplateService.content = "views/login.html";
+  TemplateService.list = 3;
 
-    $scope.navigation = NavigationService.getnav();
-    $.jStorage.flush();
-    $scope.isValidLogin = 1;
-    $scope.login = {};
-    $scope.verifylogin = function () {
-        console.log($scope.login);
-        if ($scope.login.email && $scope.login.password) {
-            NavigationService.adminLogin($scope.login, function (data, status) {
-                if (data.value == "false") {
-                    $scope.isValidLogin = 0;
-                } else {
-                    $scope.isValidLogin = 1;
-                    $.jStorage.set("adminuser", data);
-                    $location.url("/home");
-                }
-            })
+  $scope.navigation = NavigationService.getnav();
+  $.jStorage.flush();
+  $scope.isValidLogin = 1;
+  $scope.login = {};
+  $scope.verifylogin = function() {
+    console.log($scope.login);
+    if ($scope.login.email && $scope.login.password) {
+      NavigationService.adminLogin($scope.login, function(data, status) {
+        if (data.value == "false") {
+          $scope.isValidLogin = 0;
         } else {
-            console.log("blank login");
-            $scope.isValidLogin = 0;
+          $scope.isValidLogin = 1;
+          $.jStorage.set("adminuser", data);
+          $location.url("/home");
         }
+      })
+    } else {
+      console.log("blank login");
+      $scope.isValidLogin = 0;
+    }
 
-    }
+  }
 });
-phonecatControllers.controller('headerctrl', function ($scope, TemplateService, $location) {
-    $scope.template = TemplateService;
-    if (!$.jStorage.get("adminuser")) {
-        $location.url("/login");
-	    
-    }
+phonecatControllers.controller('headerctrl', function($scope, TemplateService, $location, $routeParams, NavigationService) {
+  $scope.template = TemplateService;
+  //  if (!$.jStorage.get("adminuser")) {
+  //    $location.url("/login");
+  //
+  //  }
 });
+
+phonecatControllers.controller('createorder', function($scope, TemplateService, NavigationService, ngDialog, $routeParams, $location) {
+  $scope.template = TemplateService;
+  $scope.menutitle = NavigationService.makeactive("Orders");
+  TemplateService.title = $scope.menutitle;
+  TemplateService.list = 2;
+  TemplateService.content = "views/createorder.html";
+  $scope.navigation = NavigationService.getnav();
+  console.log($routeParams.id);
+
+  $scope.order = {};
+
+  $scope.submitForm = function() {
+    console.log($scope.order);
+    NavigationService.saveOrder($scope.order, function(data, status) {
+      console.log(data);
+      $location.url("/order");
+    });
+  };
+
+
+  $scope.order.tag = [];
+  $scope.ismatch = function(data, select) {
+    abc.select = select;
+    _.each(data, function(n, key) {
+      if (typeof n == 'string') {
+        var item = {
+          _id: _.now(),
+          name: _.capitalize(n),
+          category: $scope.artwork.type
+        };
+        NavigationService.saveTag(item, function(data, status) {
+          if (data.value == true) {
+            item._id = data.id;
+          }
+        });
+        select.selected = _.without(select.selected, n);
+        select.selected.push(item);
+        $scope.order.tag = select.selected;
+      }
+    });
+    console.log($scope.artwork.tag);
+  }
+
+
+  $scope.refreshOrder = function(search) {
+    $scope.tag = [];
+    if (search) {
+      NavigationService.findArtMedium(search, $scope.order.tag, function(data, status) {
+        $scope.tag = data;
+      });
+    }
+  };
+
+  $scope.GalleryStructure = [{
+    "name": "name",
+    "type": "text",
+    "validation": [
+      "required",
+      "minlength",
+      "min=5"
+    ]
+  }, {
+    "name": "image",
+    "type": "image"
+  }, {
+    "name": "name",
+    "type": "text",
+    "validation": [
+      "required",
+      "minlength",
+      "min=5"
+    ]
+  }];
+
+  $scope.persons = [{
+    "id": 1,
+    "name": "first option"
+  }, {
+    "id": 2,
+    "name": "first option"
+  }, {
+    "id": 3,
+    "name": "first option"
+  }, {
+    "id": 4,
+    "name": "first option"
+  }, {
+    "id": 5,
+    "name": "first option"
+  }];
+
+  NavigationService.getUser(function(data, status) {
+    $scope.persons = data;
+  });
+
+});
+
+
+
+
+ //Add New Controller
--- js/navigation.js
+++ js/navigation.js
@@ -1,53 +1,55 @@
-//var adminurl = "http://localhost:1337/";
-var adminurl = "http://104.197.23.70/";
+var adminurl = "http://localhost:1337/";
+//var adminurl = "http://104.197.23.70/";
 //var adminurl = "http://192.168.2.22:1337/";
 var adminlogin = {
-    "username": "admin@admin.com",
-    "password": "admin123"
+  "username": "admin@admin.com",
+  "password": "admin123"
 };
 var navigationservice = angular.module('navigationservice', [])
 
-.factory('NavigationService', function ($http) {
-    var navigation = [{
-        name: "Dashboard",
-        classis: "active",
-        link: "#/home",
-        subnav: []
-    }];
+.factory('NavigationService', function($http) {
+  var navigation = [{
+      name: "Dashboard",
+      classis: "active",
+      link: "#/home",
+      subnav: []
+    }, 
+				//Add New Left
+  ];
 
-    return {
-        makeactive: function (menuname) {
-            for (var i = 0; i < navigation.length; i++) {
-                if (navigation[i].name == menuname) {
-                    navigation[i].classis = "active";
-                } else {
-                    navigation[i].classis = "";
-                }
-            }
-            return menuname;
-        },
-        getnav: function () {
-            return navigation;
-        },
-        adminLogin: function (data, callback) {
-            $http({
-                url: adminurl + "user/adminlogin",
-                method: "POST",
-                data: {
-                    "email": data.email,
-                    "password": data.password
-                }
-            }).success(callback);
-        },
-        countUser: function (callback) {
-            $http.get(adminurl + "user/countusers").success(callback);
-        },
-        setUser: function (data) {
-            $.jStorage.set("user", data);
-        },
-        getUser: function () {
-            $.jStorage.get("user");
-        },
-	    //Add New Service
-    }
-});
+  return {
+    makeactive: function(menuname) {
+      for (var i = 0; i < navigation.length; i++) {
+        if (navigation[i].name == menuname) {
+          navigation[i].classis = "active";
+        } else {
+          navigation[i].classis = "";
+        }
+      }
+      return menuname;
+    },
+    getnav: function() {
+      return navigation;
+    },
+    adminLogin: function(data, callback) {
+      $http({
+        url: adminurl + "user/adminlogin",
+        method: "POST",
+        data: {
+          "email": data.email,
+          "password": data.password
+        }
+      }).success(callback);
+    },
+    //    countUser: function(callback) {
+    //      $http.get(adminurl + "user/countusers").success(callback);
+    //    },
+    setUser: function(data) {
+      $.jStorage.set("user", data);
+    },
+    getUser: function() {
+      $.jStorage.get("user");
+    }, //Add New Service
+
+  }
+})

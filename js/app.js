4584c35fcff4d3c552bec882509c5181d18989a5
vigkast <vigwohlig@gmail.com>
2015-10-02 17:04:24 +0530

    double filters removed

--- js/app.js
+++ js/app.js
@@ -116,38 +116,4 @@ firstapp.filter('touppercase', function() {
     var remaining = input.substr(1);
     return firstletter.toUpperCase() + remaining;
   };
-})
-
-firstapp.filter('uploadpath', function() {
-  return function(input) {
-    return adminurl + "user/resize?file=" + input;
-  };
-});
-firstapp.directive('capitalizeFirst', function($parse) {
-  return {
-    require: 'ngModel',
-    link: function(scope, element, attrs, modelCtrl) {
-      var capitalize = function(inputValue) {
-        if (inputValue === undefined) {
-          inputValue = '';
-        }
-        var capitalized = inputValue.charAt(0).toUpperCase() +
-          inputValue.substring(1);
-        if (capitalized !== inputValue) {
-          modelCtrl.$setViewValue(capitalized);
-          modelCtrl.$render();
-        }
-        return capitalized;
-      }
-      modelCtrl.$parsers.push(capitalize);
-      capitalize($parse(attrs.ngModel)(scope)); // capitalize initial value
-    }
-  };
-});
-firstapp.filter('touppercase', function() {
-  return function(input) {
-    var firstletter = input.substr(0, 1);
-    var remaining = input.substr(1);
-    return firstletter.toUpperCase() + remaining;
-  };
 });
